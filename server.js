bb9fa61343175aa612b64d427e159b94bfba96a0
Shannon <shogeboom187@gmail.com>
2023-07-26 09:53:47 -0700

    added viewEmployee function

--- server.js
+++ server.js
@@ -83,3 +83,17 @@ const start = () => {
     });
 };
 
+// this function will display all employees
+const viewEmployees = () => {
+    const query = 'SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name AS department, CONCAT(manager.first_name, " ", manager.last_name) AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id';
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        start();
+    }); 
+}
+
+
+
+
+
