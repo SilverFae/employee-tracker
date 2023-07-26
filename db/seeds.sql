55b60b954b38afbea6a0673a9a9f43d9359458cb
Shannon <shogeboom187@gmail.com>
2023-07-26 09:37:19 -0700

    added my seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,31 @@
-    INSERT INTO employee (employee.first_name, employee.last_name, employee.role_id, employee.manager_id)
-    VALUES ("John", "Doe", 1, NULL), ("Raven", "Hogeoom", 2, )
-          
+INSERT INTO department(department_name) VALUES('Sales'), ('Engineering'), ('Finance'), ('Legal');
+
+INSERT INTO role(title, salary,department_id)
+ VALUES('Engineer', 1000000, 2), 
+
+('Sales Lead', 100000, 1), 
+
+('Lawyer', 200000, 4), 
+
+('Accountant', 80000, 3),
+
+('Salesperson', 80000, 1), 
+
+('Lead Engineer', 150000, 2), 
+
+('Account Manager', 120000, 3);
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES('Raven', 'Hoge', 1, 2),
+
+VALUES('Winnie', 'Winston', 6, null),
+
+VALUES('Emrys', 'Doe', 2, null),
+
+VALUES('Luna', 'Lominsa', 5, 3),
+
+VALUES('Ferya', 'Isla', 7, null),
+
+VALUES('Nymeria', 'Stark', 4, 5),
+
+VALUES('Harry', 'Potter', 3, null),
